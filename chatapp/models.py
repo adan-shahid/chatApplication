01d2a921416e822bf59d1f3deb3f482d91c9eed8
adan-shahid <adanshahid.student@gmail.com>
2025-07-10 14:19:17 +0500

    Displayed list of rooms in html page

--- chatapp/models.py
+++ chatapp/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class chatRooms(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True) #WE USE THIS TO CONNECT DIFFERENT SOCKETS.
+    
+    def __str__(self):
+        return self.name
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import chatRooms
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'chatapp/index.html')
+    chatrooms = chatRooms.objects.all()
+    context = {
+        'chatrooms':chatrooms,    
+    }
+    return render(request, 'chatapp/index.html', context)
