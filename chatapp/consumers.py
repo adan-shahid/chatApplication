5c6bcebc0d5a17220a207c28781944f52d07c8ba
Adan Shahid <adanshahid.student@gmail.com>
2025-11-02 19:32:06 +0500

    recieved the message in socket and sent it back. Now we will display the message int he front end

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -21,3 +21,35 @@ class chatConsumer(AsyncWebsocketConsumer):
             self.channel_layer,
             self.room_group_name
         )
+
+# HERE WE ARE RECIEVING THE MESSAGE FROM THE FRONTEND
+    async def recieve(self, message_data):
+        # LOAD THE MESSAGE DATA, IN ORDER TO DECODE IT
+        data = json.loads(message_data) 
+        # EXTRAACT DATA FROM MESSAGE/
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        # NOW,WE SEND THIS DATA TO THE CHANNEL LAYER.
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+                'type':'chat_message',
+                'message':message,
+                'username':username,
+                'room':room,
+            }
+        )
+
+# HERE WE ARE SENDING THE MESSAGE AGAIN TO THE CLIENT. BECAUSE WE HAVE MULTIPLE CLIENTS.
+#   
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dump({
+            'message':message,
+            'username':username,
+            'room':room,
+        }))
